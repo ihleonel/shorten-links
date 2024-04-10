8291b2bcf3c686edb2f116ee54dde3672888d1ef
ihleonel <ibarra.h.leonel@gmail.com>
2024-04-09 21:40:36 -0300

    Add: Form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <script setup>
+import { reactive } from 'vue'
+const form = reactive({
+  link: ''
+})
+const submit = () => {
+  console.log(form.link)
+}
 </script>
 
 <template>
   Shorten Link
+  <form @submit.prevent="submit">
+    <input
+      v-model="form.link"
+      type="text"
+      placeholder="Insert link"
+    />
+    <button
+      type="submit"
+      title="Generate"
+    >
+      Generate
+    </button>
+  </form>
 </template>
