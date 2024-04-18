507a485b65e6174ffc20f2c1008b1965a2b0002b
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2024-04-17 23:42:35 -0300

    Add: first api

--- shorter_url/urls.py
+++ shorter_url/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .users import api as users_api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', users_api.index),
 ]
