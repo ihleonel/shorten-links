89dc75f1be1cdb72365f082d2e6b060b1be9ded8
ihleonel <ibarra.h.leonel@gmail.com>
2024-04-21 21:14:20 -0300

    Delete unnecesary test

--- src/tests/shortener.test.js
+++
@@ -1,6 +0,0 @@
-import { expect, test } from 'vitest'
-import { shortener } from '../utilities/shortener'
-
-test('should shorten a link', () => {
-  expect(shortener('link')).toBe('link')
-})
--- src/utilities/shortener.js
+++
@@ -1,7 +0,0 @@
-const shortener = (link) => {
-  return link
-}
-
-export {
-  shortener
-}
