432cba8fa1ae00d7a4970c698b025554e182efb8
ihleonel <ibarra.h.leonel@gmail.com>
2024-07-24 23:53:30 -0300

    Improve component

--- frontend/src/Components/InputText.vue
+++ frontend/src/Components/InputText.vue
@@ -1,4 +1,9 @@
 <script setup>
+import defineProps from 'vue'
+const props = defineProps({
+  modelValue: String
+})
+const emit = defineEmits(['update:modelValue'])
 </script>
 <template>
   <input type="text">
